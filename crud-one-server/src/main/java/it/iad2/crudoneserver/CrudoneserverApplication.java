77f1916367f746af38eaaf449af731458e733f69
Roberta Conti <r.conti70@gmail.com>
2021-03-13 09:32:46 +0100

    Creazione progetto server

---
+++ crud-one-server/src/main/java/it/iad2/crudoneserver/CrudoneserverApplication.java
@@ -0,0 +1,13 @@
+package it.iad2.crudoneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudoneserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudoneserverApplication.class, args);
+	}
+
+}
